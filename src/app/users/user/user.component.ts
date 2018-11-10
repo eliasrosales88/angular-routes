fda601ab568ce151a768368de8dc98d4691eec7d
Elias Rosales <eliasrosales88@gmail.com>
2018-11-10 17:27:11 +0100

    Adding unsubscribe OnDestroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,7 +19,7 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     var self = this;
-    this.route.params.subscribe(function(params: Params){
+    this.paramsSubscription = this.route.params.subscribe(function(params: Params){
       self.user.id = params['id'];
       self.user.name = params['name'];
       
@@ -27,4 +29,9 @@ export class UserComponent implements OnInit {
     // this.user.name = this.route.params['name'];
   }
 
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.paramsSubscription.unsubscribe();
+  }
 }
